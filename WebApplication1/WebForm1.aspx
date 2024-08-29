cb44c6b2d995fdcc035b1aef4530eca8e0a80f65
Siva Kumar Kolli <sivakolli7482@gmail.com>
2024-08-29 20:24:14 +0530

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>29-08-2024</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+</head>
+<body>
+    <form id="form1" runat="server" class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <asp:Label ID="lblUsername" runat="server" Text="Username" CssClass="form-label"></asp:Label>
+                    <asp:TextBox ID="username" runat="server" placeholder="Enter Username" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mb-3">
+                    <asp:Label ID="lblPassword" runat="server" Text="Password" CssClass="form-label"></asp:Label>
+                    <asp:TextBox ID="password" runat="server" placeholder="Enter Password" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mb-3 text-center">
+                    <asp:Button ID="btnClr" runat="server" Text="Update" OnClick="btnClear" CssClass="btn btn-primary" ></asp:Button>
+                     <asp:Button ID="btnSub" runat="server" Text="Submit" OnClick="btnSubmit" CssClass="btn btn-primary" ></asp:Button>
+                </div>
+                <div class="mb-3 text-center">
+                    <asp:Label ID="Msg" runat="server" ForeColor="Red"></asp:Label>
+                </div>
+                <asp:GridView ID="grdData" runat="server" CssClass="table table-striped table-bordered" AutoGenerateColumns="false">
+                    <Columns>
+                        <asp:BoundField DataField="UID" HeaderText="UID" ReadOnly="True" SortExpression="UID" />
+                        <asp:BoundField DataField="username" HeaderText="Username" SortExpression="username" />
+                        <asp:BoundField DataField="password" HeaderText="Password" SortExpression="password" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridData();
+        }
+        private void GridData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=SIVA;Initial Catalog=userDB;Integrated Security=True;Trusted_Connection=True");
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from userdetails order by UID", con);
+            DataSet ds=new DataSet();
+            da.Fill(ds);
+            grdData.DataSource = ds;
+            Msg.Text = "";
+        }
+        protected void btnSubmit(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = SIVA; Initial Catalog = userDB; Integrated Security = True; Trusted_Connection = True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into userDetails values ('"+username.Text+"','"+password.Text+"')", con);
+            cmd.ExecuteNonQuery();
+            username.Text = "";
+            password.Text = "";
+            GridData();
+            Msg.Text = "Record Inserted";
+            con.Close();
+        }
+        protected void btnClear(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = SIVA; Initial Catalog = userDB; Integrated Security = True; Trusted_Connection = True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update userDetails set password='"+password.Text+"' where username='"+username.Text+"'", con);
+            cmd.ExecuteNonQuery();
+            username.Text = "";
+            password.Text = "";
+            GridData();
+            Msg.Text = "Record Updated";
+            con.Close();
+        }
+        //protected void LoginBtn(object sender, EventArgs e)
+        //{
+        //    if ((username.Text=="siva") && (password.Text == "123"))
+        //    {
+        //        Response.Redirect("someone.aspx");
+        //    }
+        //    else
+        //    {
+        //        Response.Redirect("fail.aspx");// msg.text="please enter valid username password";
+         //                                        //msg.ForeColor=System.Drawing.Color.Red;
+        //    }
+        //}
+
+    }
+}
